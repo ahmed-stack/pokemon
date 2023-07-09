10bd1eea9b67e8aac9d2ce6919cd24329a8a5036
ahmed-stack <ahmed.jamil28@gmail.com>
2023-07-09 01:31:14 +0100

    added chatbot

--- src/App.js
+++ src/App.js
@@ -20,6 +20,14 @@ function App() {
       <BrowserRouter>
         <AppLoader loadingRef="app.loading">
           <Layout>
+            <div
+            className="chatbot"
+              dangerouslySetInnerHTML={{
+                __html: `
+         <iframe src="https://web.powerva.microsoft.com/environments/Default-471dddb3-c5b4-4820-b895-73aa6d7ef7d7/bots/crb7c_azbotPatientAssisstant/webchat?__version__=2" frameborder="0" style="width: 500px; height: 1000px; background-color: white"></iframe>
+          `,
+              }}
+            />
             <Routes>
               <Route path="/" element={<PokemonPage />} />
               <Route path="/favourites" element={<PokemonPage />} />
